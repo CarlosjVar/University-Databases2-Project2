9e7aa63e8104fd678d3e971abbfdbf24d66bac9f
Carlos José Varela <carlosjvare@gmail.com>
2021-01-10 16:44:43 -0600

    R custom function

--- DataMart/R Scripts/LearningR.R
+++ DataMart/R Scripts/LearningR.R
@@ -3,7 +3,7 @@ setwd("D:\\Universidad\\University-Databases2-Project2\\DataMart")
 
 #install.packages("psych")
 #install.packages("dplyr")
-
+install.packages("tis")
 library("psych")
 library("dplyr")
 classes_player_info = c('V1'='integer','V2'='Date','V3'='integer','V4'='integer')
@@ -20,5 +20,22 @@ tail(player,n=4)
 str(player)
 summary(player)
 describe(player)
+
+
+myfunc <- function(vec){
+  x=0
+  for (i in 1:(length(vec) - 1)) {
+    
+  }
+  
+}
+  
+
+
+
 aggregate(player_info$V3,list(player_info$V1) ,FUN=sum)
+
+print("pepito")
+
+
 
