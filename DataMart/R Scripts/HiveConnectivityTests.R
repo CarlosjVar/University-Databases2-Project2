38b948dba43224f25b9b5ca4e968bc79c08bacf2
Jorge Gutiérrez Cordero <jorge.guti.estudiantil@gmail.com>
2021-01-13 16:59:42 -0600

    testing script update

--- DataMart/R Scripts/HiveConnectivityTests.R
+++ DataMart/R Scripts/HiveConnectivityTests.R
@@ -1,12 +1,17 @@
 setwd("C:\\Applications\\GitHub\\University-Databases2-Project2\\DataMart")
 
 install.packages("RJDBC")
+install.packages("rJava")
+install.packages("DBI")
+
+library(DBI)
 library(rJava)
 library(RJDBC)
 
+
 hive_jdbc_jar <- '/Connection drivers/hive-jdbc-3.1.2-standalone.jar'
 hive_driver <- 'org.apache.hive.jdbc.HiveDriver'
-hive_url <- 'jdbc:hive2://localhost:10000/default'
+hive_url <- 'jdbc:hive2://localhost:10000/ti'
 drv <- JDBC(hive_driver, hive_jdbc_jar)
 conn <- dbConnect(drv, hive_url)
 show_databases <- dbGetQuery(conn, "show databases")
