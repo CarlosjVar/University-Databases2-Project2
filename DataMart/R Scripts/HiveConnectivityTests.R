c2a4fef02d052c6f49f7d32587f04377528bac9c
Jorge Gutiérrez Cordero <jorge.guti.estudiantil@gmail.com>
2021-01-13 16:51:59 -0600

    Connection to hive testing script + cleaning of unnecessary files.

---
+++ DataMart/R Scripts/HiveConnectivityTests.R
@@ -0,0 +1,14 @@
+setwd("C:\\Applications\\GitHub\\University-Databases2-Project2\\DataMart")
+
+install.packages("RJDBC")
+library(rJava)
+library(RJDBC)
+
+hive_jdbc_jar <- '/Connection drivers/hive-jdbc-3.1.2-standalone.jar'
+hive_driver <- 'org.apache.hive.jdbc.HiveDriver'
+hive_url <- 'jdbc:hive2://localhost:10000/default'
+drv <- JDBC(hive_driver, hive_jdbc_jar)
+conn <- dbConnect(drv, hive_url)
+show_databases <- dbGetQuery(conn, "show databases")
+
+show_databases
