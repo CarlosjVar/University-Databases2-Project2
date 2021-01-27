8a67aa85bec6795a5ba8b501a308164c09be74a3
Carlos José Varela <carlosjvare@gmail.com>
2021-01-27 00:06:19 -0600

    2 Step frontend initial implementation

--- Visualization/Test/server.js
+++ Visualization/Test/server.js
@@ -4,11 +4,18 @@ const path = require('path');
 
 var app = express();
 const port= 8080;
-app.use(express.static(__dirname))
-app.get('/',(req,res)=>
+app.use('/sendFile',express.static(__dirname+'/WebFiles'))
+// app.get('/sendFile',(req,res)=>
+// {
+//     res.sendFile(path.join(__dirname,'/index.html'));
+// })
+app.use('/',express.static(__dirname+'/templates')
+)
+app.get('/go',(req,res)=>
 {
-    res.sendFile(path.join(__dirname,'/index.html'));
+    res.redirect('sendFile')
 })
+
 app.listen(port,()=>
     {
         console.log(`Listening on port ${port}`);
---
+++ Visualization/Test/templates/index.js
@@ -0,0 +1,5 @@
+document.getElementById("File").addEventListener("click", function(){
+    var cantidad = 5
+    console.log(cantidad);
+    location.assign('/sendFile');
+  })
